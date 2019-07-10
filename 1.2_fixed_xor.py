f8c2b6ded825ff8059b434caf3a84487c25829b0
jjf539 <jared.john.french@gmail.com>
2019-07-10 16:13:04 -0700

    Create 1.2_fixed_xor.py

---
+++ 1.2_fixed_xor.py
@@ -0,0 +1,19 @@
+def fixed_xor(str1,str2):
+
+  #convert hex strings to their integer equivalent
+  int_str1 = int(str1,16)
+  int_str2 = int(str2,16)
+
+  #xor str1 against str2 with xor (^) operator
+  xord = int_str1 ^ int_str2
+
+  #convert xord variable back to hex, and strip of the first two characters (0x)
+  #compare this value to the value given by the challenge
+  if hex(xord)[2:] == "746865206b696420646f6e277420706c6179":
+    print("Success!")
+    return
+  else:
+    print("Failed...")
+
+
+fixed_xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
